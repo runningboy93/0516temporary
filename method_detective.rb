25598a9d6dde0242392c9ffdb65ad8693a21f677
Christopher Beauchamp <cjbeauchamp@mac.com>
2016-05-16 01:47:34 -0500

    final commit

---
+++ method_detective.rb
@@ -0,0 +1,38 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+puts "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
+
+puts "zom".insert(1, "o")
+# => “zoom”
+
+puts "enhance".center(18)
+# => “    enhance    ”
+
+puts "Stop! You’re under arrest!".upcase
+# => “STOP! YOU’RE UNDER ARREST!”
+
+puts "the usual".concat(" suspects")
+puts "the usual" << " suspects"
+# => “the usual suspects”
+
+puts "The case of the disappearing last letter".chop
+# => “The case of the disappearing last lette”
+
+puts "The mystery of the missing first letter".reverse.chop.reverse
+# puts "The mystery of the missing first letter".[1..-1]
+# => “he mystery of the missing first letter”
+
+puts "Elementary  ,    my   dear      Watson!".gsub(/\s{2,}/, " ").gsub(/\ ,/, ",")
+# “Elementary, my dear Watson!”
+
+puts "z".ord
+# => 122
+# (What is the significance of the number 122 in relation to the character z?)
+
+puts "How many times does the letter ‘a’ appear in this string?".count("a")
+# =>4
