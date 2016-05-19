c63a55e87b7ed38ec529a010ca9eb36195a4833d
Christopher Beauchamp <cjbeauchamp@mac.com>
2016-05-19 13:07:28 -0500

    final commit

---
+++ puppy.rb
@@ -0,0 +1,21 @@
+#6.1 Release 0
+
+# Species
+# -------
+# PupicusMaximus
+
+# Characteristics
+
+# leg_count: 4
+# tail: yes
+# color: varies
+# curious: yes
+# drowsiness: extreme
+
+# Behavior
+
+# eat
+# sleep
+# flop
+# explore
+# play
