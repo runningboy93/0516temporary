72a3552bb5e4474dce8ebd5ed26f2307db62f92c
Christopher Beauchamp <cjbeauchamp@mac.com>
2016-05-21 08:48:12 -0500

    Release 0 Instructions

---
+++ ruby/vampcodepart2.rb
@@ -0,0 +1,11 @@
+
+#   elsif agecalc != old
+#     Likes_Garlic_Bread == false || Likes_healthcare == false
+#     p 'Probably a Vampire.'
+#   elsif agecalc != old
+#     Likes_Garlic_Bread == false && Likes_healthcare == false
+#     p 'Almost Certainly a Vampire.'
+#   elsif fulnam == "Drake Cula" || fulnam == "Tu Fang"
+#     p 'Definitely a Vampire.'
+#   else
+#    p 'Results Inconclusive.'
